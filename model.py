86e3e90a1db3cfca9aad89130e09a4c34ec46b2a
Phan Thang <thangphan0168@gmail.com>
2022-05-01 09:49:37 +0700

    Define model and debug train model with batch size = 1

---
+++ model.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+from transformers import AutoModelForTokenClassification
+
+
+class QASpanDetector(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = AutoModelForTokenClassification.from_pretrained('bert-base-uncased', num_labels=2)
+
+
+if __name__ == "__main__":
+    model = QASpanDetector()
+    print(model)
