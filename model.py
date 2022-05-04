f46433faf335653722dc6bc7b37dffb481fa9f5e
Phan Thang <thangphan0168@gmail.com>
2022-05-04 14:50:20 +0700

    Create dynamic label and add evaluate notebook

--- model.py
+++ model.py
@@ -7,6 +7,11 @@ class QASpanDetector(nn.Module):
         super().__init__()
         self.model = AutoModelForTokenClassification.from_pretrained('bert-base-uncased', num_labels=2)
 
+    def forward(self, data):
+        outputs = self.model(input_ids=data['input_ids'],
+                             attention_mask=data['attention_mask'])
+        return outputs
+
 
 if __name__ == "__main__":
     model = QASpanDetector()
