Detect Bug: can not backprop regression loss

model.py
@@ -7,9 +7,9 @@ class QASpanDetector(nn.Module):
         super().__init__()
         self.model = AutoModelForTokenClassification.from_pretrained('bert-base-uncased', num_labels=2)
 
-    def forward(self, data):
-        outputs = self.model(input_ids=data['input_ids'],
-                             attention_mask=data['attention_mask'])
+    def forward(self, input_ids, attention_mask):
+        outputs = self.model(input_ids=input_ids,
+                             attention_mask=attention_mask)
         return outputs
 
 